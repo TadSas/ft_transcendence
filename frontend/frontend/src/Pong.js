42d6b492cb00027d12e9f46bd97a12d8358ada2b
davitmartirosyan <phpencoding@gmail.com>
2023-04-26 03:04:35 +0400

    Frontend React Auth Page

---
+++ frontend/frontend/src/Pong.js
@@ -0,0 +1,12 @@
+import './css/Pong.css';
+import Auth from './components/Auth';
+
+function Pong() {
+  return (
+    <div className="Pong">
+		<Auth />
+    </div>
+  );
+}
+
+export default Pong;
