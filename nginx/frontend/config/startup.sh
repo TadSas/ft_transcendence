92fd3ccc49337431ec1bab5db42aeb82aa3af905
TadSas <sasun2000@gmail.com>
2024-01-03 19:08:19 +0400

    Started development of experimental pong game

--- nginx/frontend/config/startup.sh
+++ nginx/frontend/config/startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Downloading the Bootstrap
+
 bs_latest_release=$(curl -s https://api.github.com/repos/twbs/bootstrap/releases/latest)
 
 bs_vversion=$(echo "$bs_latest_release" | grep -oP '"tag_name": "\K(.*?)(?=")')
@@ -20,6 +22,8 @@ mv bootstrap-$bs_version-dist bootstrap
 
 echo "Bootstrap $bs_vversion has been downloaded and extracted."
 
+# Downloading the Bootstrap icons
+
 icons_latest_release=$(curl -s https://api.github.com/repos/twbs/icons/releases/latest)
 
 icons_vversion=$(echo "$icons_latest_release" | grep -oP '"tag_name": "\K(.*?)(?=")')
@@ -37,5 +41,27 @@ rm bootstrap-icons-$icons_version.zip
 mv bootstrap-icons-$icons_version bootstrap/icons
 
 echo "Bootstrap icons $icons_vversion has been downloaded and extracted."
+
+# Downloading the Three.js
+
+threejs_latest_release=$(curl -s https://api.github.com/repos/mrdoob/three.js/releases/latest)
+
+threejs_version=$(echo "$threejs_latest_release" | grep -oP '"tag_name": "\K(.*?)(?=")')
+
+threejs_download_url="https://github.com/mrdoob/three.js/archive/refs/tags/$threejs_version.zip"
+
+wget "$threejs_download_url" -O "three.js-$threejs_version.zip"
+
+unzip three.js-$threejs_version.zip
+
+rm three.js-$threejs_version.zip
+
+rm -rf threejs
+
+mv three.js-$threejs_version threejs
+
+echo "Three.js $threejs_version has been downloaded and extracted."
+
+# Starting the nginx service
 
 nginx -g "daemon off;"
--- nginx/frontend/static/js/views/Dashboard.js
+++ nginx/frontend/static/js/views/Dashboard.js
@@ -1,5 +1,7 @@
 import BaseView from "./BaseView.js"
 
+import * as THREE from '/threejs/build/three.module.js';
+
 
 export default class extends BaseView {
   constructor(params) {
@@ -8,6 +10,45 @@ export default class extends BaseView {
   }
 
   async getContent(routes, match) {
+
+    // Experimental game engine
+    
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+
+    const renderer = new THREE.WebGLRenderer()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.getElementById('game').appendChild(renderer.domElement)
+    
+    function addCube(x, y, w, h) {
+      const geometry = new THREE.BoxGeometry(1, 1, 1)
+      const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
+      const cube = new THREE.Mesh(geometry, material)
+      cube.position.set(x, y, 0)
+      cube.scale.set(w, h, 1)
+      scene.add(cube)
+
+      return cube
+    }
+    
+    // let paddle // player paddle
+    // let wall // top wall
+    let ball
+    
+    function init() {
+      camera.position.z = 5
+      ball = addCube(0, 0, 1, 1)
+    }
+    init()
+
+    function animate() {
+      requestAnimationFrame(animate)
+      renderer.render(scene, camera)
+    }
+    animate()
+
+    // End of experimental game engine
+
     this.getBase(routes, match)
 
     return `
